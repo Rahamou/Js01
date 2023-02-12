36ea05afd432e42ce74f643a06ce32bccf13bfe8
rahamourabe@gmail.com <rahamourabe@gmail.com>
2023-02-13 00:34:24 +0100

    team leader

--- numberToString.js
+++ numberToString.js
@@ -1,8 +1,17 @@
-const number = "[1,2,3,4,5,6,7,8,9,10]";
-const frenchnumber ='["un", "deux", "trois", "quatre", "cinq", "six", "sept", "huit", "neuf", "dix"]';
-function numberToString(){
-        console.log("Entrée :",number);
-        console.log("Sortie :",frenchnumber);
+function numberToString(moiMeme) {
+    let noms = ["un", "deux", "trois", "quatre", "cinq", "six", "sept", "huit", "neuf", "dix"];
+    let sortie = [];
+
+    for (let i = 0; i < moiMeme.length; i++) {
+        if (moiMeme[i] >= 1 && moiMeme[i] <= 10) {
+            sortie.push(noms[moiMeme[i] - 1]);
+        }
+    }
+
+    return sortie;
 }
 
-//modulo.exports=numberToString
+let moiMeme = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(numberToString(moiMeme));
+
+module.exports = numberToString;
