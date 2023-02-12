f9e0e3ba3cd9e27ab879899fde61216868adb7dd
rahamourabe@gmail.com <rahamourabe@gmail.com>
2023-02-12 23:58:38 +0100

    Js02

---
+++ numberToString.js
@@ -0,0 +1,12 @@
+const number = "[1,2,3,4,5,6,7,8,9,10]";
+const frenchnumber ='["un", "deux", "trois", "quatre", "cinq", "six", "sept", "huit", "neuf", "dix"]';
+function numberToString(){
+        console.log("Entrée :",number);
+        console.log("Sortie :",frenchnumber);
+}
+
+
+//modulo.exports = numberToString
+
+
+
