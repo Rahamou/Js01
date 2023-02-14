2a1e81c53310a5a2961b90f97bcc15fc3e2400e4
rahamourabe@gmail.com <rahamourabe@gmail.com>
2023-02-14 17:03:01 +0100

    Js00

--- displayBookInfo.js
+++ displayBookInfo.js
@@ -1,16 +1,8 @@
-//objet et échappement 
 let book = {
   "titre" : "L'Enfant Noir",
   "auteur" : "Camara Laye",
   "année" : 1953
 }
- 
- 
-function displayBookInfo(book){
-  
     console.log("Titre: " ,book.titre,);
     console.log("Auteur: ", book.auteur);
     console.log("année: ", book.année );
-}
-
-displayBookInfo (book)
--- displayPersonInfo.js
+++ displayPersonInfo.js
@@ -1,12 +1,8 @@
-// objet
+
 let personne = {
   "nom" : "Mariam",
   "âge" : 30
   }
-  
-  
-  function displayPersonInfo(personne){
     console.log("nom: ", personne.nom);
     console.log("Age: ", personne.âge);
-}
-displayPersonInfo(personne);
+
